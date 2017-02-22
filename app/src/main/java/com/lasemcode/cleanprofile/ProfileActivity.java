a637f0ac5cf55021f2389f1c15d7dda99eed152c
Lasem Code <miftakhul.miftakhulhudha@gmail.com>
2017-02-22 22:28:39 +0700

    first commit

---
+++ app/src/main/java/com/lasemcode/cleanprofile/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.lasemcode.cleanprofile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lasemcode on 22/02/17.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+    }
+}
